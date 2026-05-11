cd73a081b80c223889d4b216f7db6327d32fb6f6
malakhamadaabdelsattar-code <malakhamadaabdelsattar@gmail.com>
2026-05-12 00:56:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ function toggleMode() {
     const element = document.body;
     element.classList.toggle("dark");
 }
+function validateLogin() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    let error = document.getElementById("errorBox");
+
+    if (user === "malak" && pass === "888") {
+        window.location.href = "shop.html";
+    } else {
+        error.innerHTML = "❌ Wrong username or password!";
+    }
+}
+ counter = 0; 
+
+function startMiniSlides() {
+    let images = document.getElementsByClassName("MySlide");
+    
+    
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";  
+    }
+    
+    counter++;
+    
+  
+    if (counter > images.length) { counter = 1; }    
+    
+    
+    if (images.length > 0) {
+        images[counter - 1].style.display = "block";  
+        setTimeout(startMiniSlides, 3000); 
+    }
+}
+
+
+window.onload = startMiniSlides;
